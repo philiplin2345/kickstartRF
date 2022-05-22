238d53e7c4c4b1b1f6300dfa7087aed27ffeb2f3
DESKTOP-1B90OF5\Philip <philip.lin@hy-tech.com.tw>
2022-05-22 13:49:08 +0800

    :zap: Lesson 185 Added user form to new campaigns page (L185 From Creation)

--- pages/campaigns/new.jsx
+++ pages/campaigns/new.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import { Button, Form } from 'semantic-ui-react'
 
 
 const CampaignNew = () => {
-  return (
-    <div>CampaignNew</div>
-  )
+    return (
+        <Form>
+            <h3>Create a Campaign!</h3>
+            <Form.Field>
+                <label>Minimum Contribution</label>
+                <input placeholder='in wei' />
+            </Form.Field>
+            
+           
+            <Button type='submit' primary>Create</Button>
+        </Form>
+    )
 }
 
 export default CampaignNew
