40962fc44d8784090983024902749f79dba49d59
DESKTOP-1B90OF5\Philip <philiplin2435@gmail.com>
2022-05-22 22:35:47 +0800

    :heavy_plus_sign: Lesson 202 Added campaign object, prerendered campaign summary (L202 Accessing a Campaign)

--- pages/campaigns/[id]/index.jsx
+++ pages/campaigns/[id]/index.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
 import { useRouter } from 'next/router';
- 
-const Campaign = () => {
-  const router = useRouter();
-  const { id } = router.query;
- 
-  return <h3>Campaign {id}</h3>;
+import EthCp from '../../../ethereum/campaign'
+
+const Campaign = (props) => {
+    const router = useRouter();
+    const { id } = router.query;
+    const jsonData = JSON.parse(props.jsonSummary);
+    console.log(jsonData[0]);
+    console.log(jsonData[1]);
+    console.log(jsonData[2]);
+    console.log(jsonData[3]);
+    console.log(jsonData[4]);
+    // console.log(props.summary)
+    return <h3>Campaign {id}</h3>;
 };
- 
+
+
+export async function getServerSideProps(context) {
+    console.log(context.query.id);
+    const cp = EthCp(context.query.id);
+    const summary = await cp.methods.getSummary().call();
+    console.log (JSON.stringify(summary))
+    const jsonSummary= JSON.stringify(summary)
+    return { props: { jsonSummary } };
+}
+
 export default Campaign;
