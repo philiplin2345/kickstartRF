c0d4f4d9242936f46e1017062baba6e897611375
DESKTOP-1B90OF5\Philip <philip.lin@hy-tech.com.tw>
2022-05-22 14:51:33 +0800

    :zap: L189 Added error message when user enters unexpected input or transation fails (L189 Form Error Handling)

--- pages/campaigns/new.jsx
+++ pages/campaigns/new.jsx
@@ -1,36 +1,45 @@
-import React, {useState} from 'react'
-import { Button, Form,Input } from 'semantic-ui-react'
+import React, { useState } from 'react'
+import { Button, Form, Input, Message } from 'semantic-ui-react'
 import factory from '../../ethereum/factory'
 import web3 from '../../ethereum/web3'
 
 
 const CampaignNew = () => {
-    const[minContribution, setMinContribution] = useState(0);
+    const [minContribution, setMinContribution] = useState(0);
+    const [errorMessage, setErrorMessage] = useState('');
 
-    const handleMinContributionChange = ()=>{
+    const handleMinContributionChange = () => {
         setMinContribution(event.target.value);
     }
 
-    const submitForm = async () =>{
+    const submitForm = async () => {
         event.preventDefault();
-        const accounts = await web3.eth.getAccounts();
-        await factory.methods.createCampaign(minContribution)
-        .send({
-            from: accounts[0]
-        });
+        try {
+            const accounts = await web3.eth.getAccounts();
+            await factory.methods.createCampaign(minContribution)
+                .send({
+                    from: accounts[0]
+                });
+        } catch (error) {
+            setErrorMessage(error.message);
+        }
         setMinContribution(0);
 
     };
 
     return (
-        <Form onSubmit = {submitForm}>
+        <Form onSubmit={submitForm} error={errorMessage?true:false}>
             <h3>Create a Campaign!</h3>
             <Form.Field>
                 <label>Minimum Contribution</label>
                 <Input label='wei' placeholder='amount' labelPosition='right' onChange={handleMinContributionChange} />
             </Form.Field>
-            
-           
+
+            <Message
+                error
+                header='There was some errors with your submission'
+                content = {errorMessage}
+            />
             <Button type='submit' primary>Create</Button>
         </Form>
     )
