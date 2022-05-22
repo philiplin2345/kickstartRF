06908d0eb3a890fba58eb565bc4daee9721b6048
DESKTOP-1B90OF5\Philip <philip.lin@hy-tech.com.tw>
2022-05-22 13:14:19 +0800

    :heavy_plus_sign: Lesson 180 Added Menu inside Header (L180. Assembling a Header)

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Menu } from 'semantic-ui-react'
+
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
+
+    const [activeItem, setActiveItem] = useState('')
+
+    const handleItemClick = (event, menuItem) => {
+        console.log(event.target);
+        console.log(menuItem.name);
+        // setActiveItem(name);
+    }
+    return (
+        <Menu>
+            <Menu.Item
+                name='CrowdCoin'
+                active={activeItem === 'CrowdCoin'}
+                onClick={handleItemClick}
+            >
+                CrowdCoin
+            </Menu.Item>
+
+            
+            <Menu.Menu position='right'>
+                <Menu.Item
+                    name='Campaigns'
+                    active={activeItem === 'Campaigns'}
+                    onClick={handleItemClick}
+                >
+                    Campaigns
+                </Menu.Item>
+
+                <Menu.Item
+                    name='plus'
+                    active={activeItem === 'plus'}
+                    onClick={handleItemClick}
+                >
+                    +
+                </Menu.Item>
+            </Menu.Menu>
+        </Menu>
+    )
 }
 
 export default Header
