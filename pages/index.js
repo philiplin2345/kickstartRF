d7dddccd404ee57f22162441802aedb875b20660
DESKTOP-1B90OF5\Philip <philip.lin@hy-tech.com.tw>
2022-05-22 10:52:01 +0800

    :heavy_plus_sign: Lesson 176 added Create Campaign button

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import factory from '../ethereum/factory'
-import { Card } from 'semantic-ui-react'
+import { Card, Button } from 'semantic-ui-react'
 import 'semantic-ui-css/semantic.min.css'
 
 const Index = (props) => {
@@ -18,17 +18,21 @@ const Index = (props) => {
     }, [])
 
     const allCampaignsItems = props.campaigns.map(address => {
-    return {
-        header: address,
-        description:
-          <a>View Campaign</a>,
-        fluid: true
-      };})
-    
-    const allCampaigns = <Card.Group items = {allCampaignsItems}/>
-    
+        return {
+            header: address,
+            description:
+                <a>View Campaign</a>,
+            fluid: true
+        };
+    })
+
+    const allCampaigns = <Card.Group items={allCampaignsItems} />
+
     return (
-        <div>{allCampaigns}</div>
+        <div>
+            {allCampaigns}
+            <Button content = 'Create Campaign' icon ='add circle' primary></Button>
+        </div>
     )
 }
 
