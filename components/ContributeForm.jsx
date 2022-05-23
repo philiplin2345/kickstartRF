471d1df9e41ee8410b8d38aa74444c0ec4b6f34a
Lamb Ball <lb@gmail.com>
2022-05-23 18:02:39 +0800

    :zap: Lesson 208 Added contribution form logic (L208 Form State)

--- components/ContributeForm.jsx
+++ components/ContributeForm.jsx
@@ -1,15 +1,25 @@
-import React from 'react'
-import {Form,Input,Message,Button} from 'semantic-ui-react'
+import React, {useState} from "react";
+import { Form, Input, Message, Button } from "semantic-ui-react";
 const ContributeForm = () => {
+    const [contributionAmount, setContributionAmount] = useState(0)
+    const handleInputChange = ()=>{
+        setContributionAmount(event.target.value)
+        console.log(contributionAmount)
+    }
+    const handleSubmit = () =>{
+        event.preventDefault();
+    }
   return (
-    <Form >
-            <Form.Field>
-                <label>Amount to Contribute</label>
-                <Input label='ether' placeholder='amount' labelPosition='right'  />
-            </Form.Field>
-            <Button type='submit' primary>Contribute!</Button>
+    <Form onSubmit = {handleSubmit}>
+      <Form.Field>
+        <label>Amount to Contribute</label>
+        <Input label="ether" placeholder="amount" labelPosition="right" onChange={handleInputChange} />
+      </Form.Field>
+      <Button type="submit" primary>
+        Contribute!
+      </Button>
     </Form>
-  )
-}
+  );
+};
 
-export default ContributeForm
+export default ContributeForm;
