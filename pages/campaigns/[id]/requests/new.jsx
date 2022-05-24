5b0292c633b9df8b76d39a8698af080526f91bb1
DESKTOP-1B90OF5\Philip <philiplin2435@gmail.com>
2022-05-24 13:43:42 +0800

    :zap: Lesson 218 Added error message and rerouting after new request submission (L218 Form Polish)

--- pages/campaigns/[id]/requests/new.jsx
+++ pages/campaigns/[id]/requests/new.jsx
@@ -1,15 +1,21 @@
 import React, { useState } from 'react';
-import { Form, Button, Messaage, Input } from 'semantic-ui-react';
+import { Form, Button, Message, Input } from 'semantic-ui-react';
 import Campaign from '../../../../ethereum/campaign';
 import web3 from '../../../../ethereum/web3';
 import Link from 'next/link';
+import { useRouter } from "next/router";
 
 const RequestNew = (props) => {
 
     const [descriptionInput, setDescriptionInput] = useState('');
     const [valueInput, setValueInput] = useState(0);
     const [recipientInput, setRecipientInput] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+    const [isProcessingTx, setIsProcessingTx] = useState(false);
+    const router = useRouter();
     const submitNewRequest = async (event) => {
+        setIsProcessingTx(true);
+        setErrorMessage('');
         event.preventDefault();
         const campaign = Campaign(props.address);
         console.log(descriptionInput, web3.utils.toWei(valueInput, 'ether'), recipientInput)
@@ -18,14 +24,24 @@ const RequestNew = (props) => {
             await campaign.methods.createRequest(
                 descriptionInput, web3.utils.toWei(valueInput, 'ether'), recipientInput
             ).send({ from: accounts[0] });
+            router.reload(`/campaigns/${props.address}/requests`)
         } catch (error) {
-
+            setErrorMessage(error.message);
         }
+        setDescriptionInput('')
+        setValueInput(0)
+        setRecipientInput('')
+        setIsProcessingTx(false);
     }
     return (
         <>
+            <Link href={`/campaigns/${props.address}/requests`} >
+                <a>
+                    Back
+                </a>
+            </Link>
             <h3>Create a Request</h3>
-            <Form onSubmit={submitNewRequest}>
+            <Form onSubmit={submitNewRequest} error={errorMessage ? true : false}>
                 <Form.Field>
                     <label>Description</label>
                     <Input value={descriptionInput} onChange={e => setDescriptionInput(e.target.value)}></Input>
@@ -38,7 +54,12 @@ const RequestNew = (props) => {
                     <label>Recipient</label>
                     <Input value={recipientInput} onChange={e => setRecipientInput(e.target.value)}></Input>
                 </Form.Field>
-                <Button primary type='submit'> Create!! </Button>
+                <Message
+                    error
+                    header='There was some errors with your submission'
+                    content={errorMessage}
+                />
+                <Button primary type='submit' loading={isProcessingTx}> Create!! </Button>
             </Form>
         </>
     )
