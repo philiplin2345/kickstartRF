241a31cac6f92f2f3f27b8c1bea96bbe4df11e1e
pl <plplpl@gmail.com>
2022-05-24 14:05:56 +0800

    :zap: Lesson 221 Got all requests data for 1 campaign (L221 Fancy Javascript)

--- pages/campaigns/[id]/requests/index.jsx
+++ pages/campaigns/[id]/requests/index.jsx
@@ -1,28 +1,40 @@
 import React from 'react';
 import { Button } from 'semantic-ui-react';
-import  Link  from 'next/link';
+import Link from 'next/link';
+import web3 from '../../../../ethereum/web3';
+import Campaign from '../../../../ethereum/campaign';
 
 const RequestIndex = (props) => {
     return (
         <>
-        <div>{props.address} requests</div>
-        <Link href={`/campaigns/${props.address}/requests/new`} >
-            <a>
-                <Button primary >Add Request</Button>
-            </a>
-        </Link>
+            <div>{props.address} requests</div>
+            <Link href={`/campaigns/${props.address}/requests/new`} >
+                <a>
+                    <Button primary >Add Request</Button>
+                </a>
+            </Link>
         </>
     )
 }
 
 export async function getServerSideProps(context) {
     console.log(context.query.id);
+    const address = context.query.id;
     console.log(context);
+    const campaign = Campaign(address);
+    const requestCount = await campaign.methods.getRequestsCount().call();
+
+    const requests = await Promise.all(
+        Array(requestCount).fill().map((element,index)=>{
+            return campaign.methods.requests(index).call();
+        })
+    );
+    console.log(requests)
     // const cp = EthCp(context.query.id);
     // const summary = await cp.methods.getSummary().call();
     return {
         props: {
-            address: context.query.id
+            address: address
             // minimumContribution: summary[0],
             // balance: summary[1],
             // requestsCount: summary[2],
